7ffa7d7b365629dd30a08dc334389a67c419d98c
Robert Schultz <robert@cosmicrealms.com>
2021-05-14 16:17:43 -0400

    Improved the output of amigaGuide conversions

--- src/program/document/grotag.js
+++ src/program/document/grotag.js
@@ -1,6 +1,9 @@
 "use strict";
 const XU = require("@sembiance/xu"),
 	path = require("path"),
+	tiptoe = require("tiptoe"),
+	domino = require("domino"),
+	fs = require("fs"),
 	fileUtil = require("@sembiance/xutil").file;
 
 exports.meta =
@@ -16,4 +19,65 @@ exports.bin = () => "grotag";
 exports.args = (state, p, r, inPath=state.input.absolute, outPath=state.output.absolute) => (["-w", inPath, outPath]);
 
 // The CSS produced by grotag includes this ugly big great outline on all links. Let's get rid of it
-exports.post = (state, p, r, cb) => fileUtil.searchReplace(path.join(state.output.absolute, "amigaguide.css"), "outline: solid gray", "", cb);
+exports.post = (state, p, r, cb) =>
+{
+	// grotag creates a seperate CSS file and all HTML files include that. We are gonna inline the CSS into each HTML file and delete the CSS file and move the resulting HTML files up a directory
+	const cssFilePath = path.join(state.output.absolute, "amigaguide.css");
+
+	tiptoe(
+		function findHTMLFiles()
+		{
+			fileUtil.glob(state.output.absolute, "**/*.html", {nodir : true}, this);
+		},
+		function loadCSSAndHTMLFiles(htmlFilePaths)
+		{
+			this.data.htmlFilePaths = htmlFilePaths;
+			fs.readFile(cssFilePath, XU.UTF8, this.parallel());
+			htmlFilePaths.parallelForEach((htmlFilePath, subcb) => fs.readFile(htmlFilePath, XU.UTF8, subcb), this.parallel());
+		},
+		function saveModifiedHTML(cssRaw, htmlFilesRaw)
+		{
+			// The CSS file often has a ton of trailing null bytes, get rid of those. Also all links have this annoying solid gray outline, get rid of that too.
+			const cssData = cssRaw.replaceAll("\0", "").trim().replaceAll("outline: solid gray", "");
+
+			fileUtil.unlink(cssFilePath, this.parallel());
+			fileUtil.unlink(path.dirname(this.data.htmlFilePaths[0]), this.parallel());
+
+			this.data.htmlFilePaths.parallelForEach((htmlFilePath, subcb, i) =>
+			{
+				const doc = domino.createWindow(htmlFilesRaw[i]).document;
+
+				// Delete any links we have to the external CSS file
+				Array.from(doc.querySelectorAll("link")).forEach(o =>
+				{
+					if((o.getAttribute("href") || "").includes("amigaguide.css"))
+						o.remove();
+				});
+
+				// Create an inline STYLE tag and insert it into HEAD
+				const style = doc.createElement("style");
+				style.textContent = cssData;
+				doc.querySelector("head").appendChild(style);	// eslint-disable-line unicorn/prefer-dom-node-append
+
+				// Properly encode any hyperlinks (to handle files that have question marks in them, etc)
+				Array.from(doc.querySelectorAll("a")).forEach(a => a.setAttribute("href", a.getAttribute("href").encodeURLPath()));
+
+				// For whatever reason, grotag doesn't properly link the Index text to index.html, so we do it ourselves here
+				const body = doc.querySelector("body");
+				Array.from(body.childNodes).forEach(o =>
+				{
+					if(o.nodeType===3 && o.textContent.includes("| Index |"))
+						o.textContent = ` | ${o.textContent.replaceAll("| Index |", "|")}`;
+				});
+
+				const topLink = doc.createElement("a");
+				topLink.setAttribute("href", "index.html");
+				topLink.textContent = "Index";
+				body.insertBefore(topLink, body.childNodes[0]);
+
+				fs.writeFile(path.join(state.output.absolute, path.basename(htmlFilePath)), doc.outerHTML, XU.UTF8, subcb);
+			}, this.parallel());
+		},
+		cb
+	);
+};

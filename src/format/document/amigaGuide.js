72eaebfd846d4b0cfb24a77c0e21397311e10fb2
Robert Schultz <robert@cosmicrealms.com>
2021-12-15 22:06:46 -0500

    Minor comment change

--- src/format/document/amigaGuide.js
+++ src/format/document/amigaGuide.js
@@ -10,7 +10,7 @@ export class amigaGuide extends Format
 	auxFiles     = (input, otherFiles, otherDirs) => ((otherFiles.length>0 || otherDirs.length>0) ? [...otherFiles, ...otherDirs] : false);	// Amiga Guides reference other guides and directories, so include symlinks to everything else
 	
 	// Grotag is best because it'll have access to the 'otherFiles' and 'otherDirs'
-	// We used to use 'guideml' but it's just waaaay too buggy and hangs on almost every guide, often locking up files on the amiga.
+	// Used to use 'guideml' but it's just waaaay too buggy and hangs on almost every guide, often locking up files on the amiga.
 	// So we just do grotag and strings now
 	converters    = ["grotag", "strings"];
 }

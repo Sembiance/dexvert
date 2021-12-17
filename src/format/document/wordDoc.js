4b5d6de999f094f0749cf29361306b541b48aa4a
Robert Schultz <robert@cosmicrealms.com>
2021-12-17 17:22:05 -0500

    Formatting and wordDoc passwordProtected better handling

--- src/format/document/wordDoc.js
+++ src/format/document/wordDoc.js
@@ -8,14 +8,13 @@ export class wordDoc extends Format
 	forbidExtMatch = true;
 	magic          = ["Microsoft Word document", "Microsoft Word for Windows"];
 	converters     = ["fileMerlin", "antiword", "soffice"];
-	post = dexState =>
+	processed      = dexState =>
 	{
 		Object.assign(dexState.meta, dexState.ran.find(({programid}) => programid==="antiword")?.meta || {});
 		if(dexState.meta.passwordProtected)
 		{
 			// can't do this in a 'untouched' callback because this meta data isn't available until after antiword converter has ran and the untouched method is called before converters
 			dexState.untouched = true;
-			dexState.processed = true;
 			return true;
 		}
 
--- src/format/text/txt.js
+++ src/format/text/txt.js
@@ -4,11 +4,11 @@ import {TEXT_MAGIC} from "../../Detection.js";
 // Fallback match for anything that is just text. This will only be matched as a last resort
 export class txt extends Format
 {
-	name = "Text File";
-	website = "http://fileformats.archiveteam.org/wiki/Text";
-	magic = TEXT_MAGIC;
-	priority = this.PRIORITY.LOWEST;
-	fallback = true;
-	untouched = true;
+	name         = "Text File";
+	website      = "http://fileformats.archiveteam.org/wiki/Text";
+	magic        = TEXT_MAGIC;
+	priority     = this.PRIORITY.LOWEST;
+	fallback     = true;
+	untouched    = true;
 	metaProvider = ["text"];
 }

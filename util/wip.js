47c6778667ba7457d6e65ed2e39cdd67e2925a1a
Robert Schultz <robert@cosmicrealms.com>
2021-12-30 22:49:56 -0500

    Updated todo file

--- util/wip.js
+++ util/wip.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
 import {xu} from "xu";
+import {XLog} from "xlog";
 import {runUtil, fileUtil} from "xutil";
 import {path, delay} from "std";
 
+const xlog = new XLog();
 
-const test = await Deno.lstat("/tmp/berjkhbkerbner").catch(() => {});
-console.log(test);

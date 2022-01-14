e0fa3d86b0f7de891be7bd2e3859d91f0a52b3ff
Robert Schultz <robert@cosmicrealms.com>
2022-01-14 14:17:12 -0500

    Tightened support for install shield compiled scripts

--- build/targets/README.js
+++ build/targets/README.js
@@ -10,11 +10,11 @@ export default async function buildREADME(xlog)
 {
 	xlog.info`Writing README.md to disk...`;
 	await Deno.writeTextFile(path.join(xu.dirname(import.meta), "..", "..", "README.md"), `# dexvert - Decompress EXtract conVERT
-Convert **${Object.keys(supportedFormats).length.toLocaleString()}** file formats (out of **${Object.keys(formats).length.toLocaleString()}** known) into modern equilivants (png/svg/pdf/mp3/mp4/etc.).
+Convert **${Object.keys(supportedFormats).length.toLocaleString()}** file formats (out of **${Object.keys(formats).length.toLocaleString()}** known) into modern equilivants (png/svg/pdf/mp3/mp4/etc.)
 
-Utilizes [Deno](https://deno.land/) to leverage **${Object.keys(programs).length.toLocaleString()}** helper programs running on **${QEMUIDS.length}** different operating systems under [QEMU](https://www.qemu.org/).
+Utilizes [Deno](https://deno.land/) to leverage **${Object.keys(programs).length.toLocaleString()}** helper programs running on **${QEMUIDS.length}** different operating systems under [QEMU](https://www.qemu.org/)
 
-See the lists of [SUPPORTED](SUPPORTED.md) & [UNSUPPORTED](UNSUPPORTED.md) file formats.
+See the lists of [SUPPORTED](SUPPORTED.md) & [UNSUPPORTED](UNSUPPORTED.md) file formats
 
 ## Install and Usage
 There is no easy way to install or use this yourself due to 3 factors:
@@ -22,5 +22,5 @@ There is no easy way to install or use this yourself due to 3 factors:
 * Over **${Object.keys(programs).length.toLocaleString()}** programs need to be installed, many with [custom code patches](https://github.com/Sembiance/dexvert-gentoo-overlay)
 * Various kernel and OS configurations need to be set just right
 
-Some day I may offer a website where you can upload a file and have it be identified and converted.`);
+Some day I may offer a website where you can upload a file and have it be identified and converted`);
 }
--- src/program/document/SID.js
+++ src/program/document/SID.js
@@ -24,33 +24,44 @@ export class SID extends Program
 			Send("c:\\in\\${path.basename(r.inFile())}{ENTER}")
 			WinWaitClose("Choose file to decompile", "", 10)
 
-			Local $winHandle = WinGetHandle("[sid] sexy installshield decompiler")
+			; Dismiss Error dialog
+			Local $errorDialog = WinWaitActive("[TITLE:Error]", "", 3)
+			If $errorDialog Not = 0 Then
+				; Can't click the OK button or close the window any other way than killing it
+				KillAll("sid.exe")
+			Else
+				Local $winHandle = WinGetHandle("[sid] sexy installshield decompiler")
 
-			; Wait for the the progress bar to fill up
-			Local $pixelColor
-			Local $timer = TimerInit()
-			Do
-				$pixelColor = PixelGetColor(1015, 729, $winHandle)
-				If Hex($pixelColor) == "00000080" Then ExitLoop
+				; Wait for the the progress bar to fill up
+				Local $pixelColor
+				Local $timer = TimerInit()
+				Do
+					$pixelColor = PixelGetColor(1015, 729, $winHandle)
+					If Hex($pixelColor) == "00000080" Then ExitLoop
+					Sleep(50)
+				Until TimerDiff($timer) > ${xu.MINUTE*2}
+
+				Sleep(${xu.SECOND*10})
+
+				ClipPut("")
+				Send("^a")
 				Sleep(50)
-			Until TimerDiff($timer) > ${xu.MINUTE*2}
+				Send("^c")
+				WaitForClipChange(${xu.SECOND})
+				FileWrite("c:\\out\\out.txt", ClipGet())
 
-			Sleep(${xu.SECOND*10})
-
-			ClipPut("")
-			Send("^a")
-			Sleep(50)
-			Send("^c")
-			WaitForClipChange(${xu.SECOND})
-			FileWrite("c:\\out\\out.txt", ClipGet())
-
-			Sleep(500)
-			Send("!f")
-			Sleep(100)
-			Send("q")
-			Sleep(100)
-			
-			WinWaitClose("[sid] sexy installshield decompiler", "", 10)`
+				Sleep(500)
+				Send("!f")
+				Sleep(100)
+				Send("q")
+				Sleep(100)
+				
+				WinWaitClose("[sid] sexy installshield decompiler", "", 10)
+			EndIf`
 	});
+
+	// If our output is exactly 166 bytes and contains the word 'Error' then something went wrong
+	verify = async (r, dexFile) => dexFile.size!==166 || !(await Deno.readTextFile(dexFile.absolute)).includes("Error");
+
 	renameOut = true;
 }

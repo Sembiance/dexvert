28ef6db37b3eb3971b7987fade5f53271105cebc
Robert Schultz <robert@cosmicrealms.com>
2022-02-01 21:00:38 -0500

    Added unar and deark as msCompound extractors

--- src/format/archive/msCompound.js
+++ src/format/archive/msCompound.js
@@ -7,5 +7,5 @@ export class msCompound extends Format
 	magic            = ["Generic OLE2 / Multistream Compound", "Composite Document File V2 Document", "OLE2 Compound Document Format"];
 	forbiddenExt     = [".fpx"];	// Allow image/fpx to handle these
 	confidenceAdjust = (input, matchType, curConfidence) => -(curConfidence-40);	// MS Word/Excel files and Thumbs.db are also Compound Documents. Usually archive/* goes first, but let's reduce confidence here so others can go first instead like document/wordDoc
-	converters       = ["sevenZip"];
+	converters       = ["sevenZip", "unar", "deark[module:cfb][opt:cfb:extractstreams]"];
 }

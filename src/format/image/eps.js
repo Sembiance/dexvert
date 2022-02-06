38c8f84a811bb6a62d0315ff452290f626acc74c
Robert Schultz <robert@cosmicrealms.com>
2022-02-05 21:50:58 -0500

    Added fallbacks for EPS

--- src/format/image/eps.js
+++ src/format/image/eps.js
@@ -11,6 +11,6 @@ export class eps extends Format
 	ext        = _EPS_EXT;
 	mimeType   = "application/eps";
 	magic      = _EPS_MAGIC;
-	notes      = "We used to convert to both PNG and SVG using nconvert & inkscape. But ps2pdf -> pdf2svg works much better and supports both raster and vector versions";
-	converters = ["ps2pdf -> pdf2svg"];
+	notes      = "We used to convert to both PNG and SVG using nconvert & inkscape. But ps2pdf -> pdf2svg works much better and supports both raster and vector versions. Still, fallback to inkscape for some files like eagle and eagle.001";
+	converters = ["ps2pdf -> pdf2svg", "inkscape", "nconvert"];
 }

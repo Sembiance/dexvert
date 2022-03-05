9c7c4dc3c03a8804c52a068942cded5f779f78b2
Robert Schultz <robert@cosmicrealms.com>
2022-03-04 22:01:55 -0500

    Added .asc as a valid ANSI file extension

--- src/format/image/ans.js
+++ src/format/image/ans.js
@@ -6,7 +6,7 @@ export class ans extends Format
 	name           = "ANSI Art File";
 	website        = "http://fileformats.archiveteam.org/wiki/ANSI_Art";
 	ext            = [".ans", ".drk", ".ice", ".ansi", ".asc"];
-	weakExt        = [".drk", ".ice"];	// .ANS was widely accepted as ANSI, but .drk and .ice less so.
+	weakExt        = [".drk", ".ice", ".asc"];	// .ANS was widely accepted as ANSI, but .drk and .ice less so. .asc is also not widely used.
 	// Sadly some .ice files like "2002 - 20 - tcf-0001.ice" won't get converted because they only identify as 'data' and since ansilive will convert any file you send into it, we can't send it these
 	forbidExtMatch = true;
 	mimeType       = "text/x-ansi";

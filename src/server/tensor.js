df94dd15b801849f24ef5f8d10cd5b5300cc4658
Robert Schultz <robert@cosmicrealms.com>
2022-10-21 00:05:21 -0400

    Only use software tensor on laptop

--- src/server/tensor.js
+++ src/server/tensor.js
@@ -32,8 +32,8 @@ export class tensor extends Server
 		}
 
 		this.xlog.info`Running tensor server docker...`;
-		const tensorDockerTag = Deno.hostname()==="lostcrag" ? "tensorflow/tensorflow:latest-gpu" : "tensorflow/tensorflow";
-		const tensorGPUArgs = Deno.hostname()==="lostcrag" ? ["--gpus", "all"] : [];
+		const tensorDockerTag = Deno.hostname()==="crystalsummit" ? "tensorflow/tensorflow" : "tensorflow/tensorflow:latest-gpu";
+		const tensorGPUArgs = Deno.hostname()==="crystalsummit" ? [] : ["--gpus", "all"];
 		const dockerArgs = ["run", "--name", "dexvert-tensor", ...tensorGPUArgs, "--rm", "-p", `${TENSORSERV_HOST}:${TENSORSERV_PORT}:${TENSORSERV_PORT}`, "-v", `${WIP_DIR}:${WIP_DIR}`, "-w", WIP_DIR, tensorDockerTag, "./tensorServer.sh"];
 		await runUtil.run("docker", dockerArgs, {verbose : true, detached : true, stdoutcb : line => this.xlog.info`${line}`, stderrcb : line => this.xlog.warn`${line}`, cwd : WIP_DIR});
 	}

b8867f29ab3119f4c5c0beb300db79a04388ceda
Robert Schultz <robert@cosmicrealms.com>
2022-10-27 14:51:00 -0400

    Tightened up matching on extension .movie for sgiMovie

--- src/format/video/sgiMovie.js
+++ src/format/video/sgiMovie.js
@@ -2,10 +2,11 @@ import {Format} from "../../Format.js";
 
 export class sgiMovie extends Format
 {
-	name         = "Silicon Graphics IRIX Movie";
-	website      = "http://fileformats.archiveteam.org/wiki/SGI_movie";
-	ext          = [".mv", ".movie", ".sgi"];
-	magic        = ["SGI video", "Silicon Graphics movie file"];
-	metaProvider = ["mplayer"];
-	converters   = ["ffmpeg", "xanim"];
+	name           = "Silicon Graphics IRIX Movie";
+	website        = "http://fileformats.archiveteam.org/wiki/SGI_movie";
+	ext            = [".mv", ".movie", ".sgi"];
+	forbidExtMatch = [".movie"];
+	magic          = ["SGI video", "Silicon Graphics movie file"];
+	metaProvider   = ["mplayer"];
+	converters     = ["ffmpeg", "xanim"];
 }

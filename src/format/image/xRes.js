e7b25f1c29fa4accc7415980516ca537deebfbe4
Robert Schultz <robert@cosmicrealms.com>
2023-01-09 21:10:57 -0500

    wip

--- src/format/image/xRes.js
+++ src/format/image/xRes.js
@@ -6,6 +6,5 @@ export class xRes extends Format
 	website     = "http://fileformats.archiveteam.org/wiki/XRes";
 	ext         = [".lrg"];
 	magic       = ["xRes multi-resolution bitmap"];
-	notes       = "Have xRes 3.0 in sandbox/app but even though it installed in both Win2k and WinXP it doesn't launch, just exits. Could try with Vista or maybe it's a lost cause. Only encountered on 1 CD so far.";
 	unsupported = true;
 }
--- src/format/music/masterTracksPro.js
+++ src/format/music/masterTracksPro.js
@@ -6,5 +6,4 @@ export class masterTracksPro extends Format
 	ext         = [".mts"];
 	magic       = ["Master Tracks Score"];
 	unsupported = true;
-	notes       = "Master Tracks Pro (app in sandbox/app/master_tracks_pro_full.exe and S/N is in email) can convert this to MIDI, but it only runs on Vista/7/8/10.";
 }

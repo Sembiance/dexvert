ed947dbf9126eee00218bdf33de0a043dbd232e3
Robert Schultz <robert@cosmicrealms.com>
2023-01-12 22:30:25 -0500

    No longer extract arArchive files, just produces .o files which are not interesting.

--- src/format/archive/arArchive.js
+++ src/format/archive/arArchive.js
@@ -2,9 +2,11 @@ import {Format} from "../../Format.js";
 
 export class arArchive extends Format
 {
-	name       = "AR Archive";
-	website    = "http://fileformats.archiveteam.org/wiki/AR";
-	ext        = [".a", ".lib"];
-	magic      = ["current ar archive", "ar archive"];
-	converters = ["deark", "ar"];
+	name        = "AR Archive";
+	website     = "http://fileformats.archiveteam.org/wiki/AR";
+	ext         = [".a", ".lib"];
+	magic       = ["current ar archive", "ar archive"];
+	unsupported = true;
+	notes       = "We used to convert with deark/ar but all that usually is stored inside is .o object which are not interesting and some .a files like libphobos2.a produce 9,999 files which is a lot of noise.";
+	//converters = ["deark", "ar"];
 }

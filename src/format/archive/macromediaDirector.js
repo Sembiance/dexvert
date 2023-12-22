05559a937b74cbe9fdfd8df28a6d42aa258a67ad
Robert Schultz <robert@cosmicrealms.com>
2023-12-21 19:10:34 -0500

    Thanks to the AWESOME Director Cast Ripper, no longer need dirOpener/drxtract/recover_cct or macromedia director itself. Woot

--- src/format/archive/macromediaDirector.js
+++ src/format/archive/macromediaDirector.js
@@ -8,28 +8,17 @@ export class macromediaDirector extends Format
 	forbidExtMatch = true;
 	magic          = ["Macromedia Director project", "Adobe Director Protected Cast", "Macromedia Director Protected Movie", "Director - Shockwave movie", "Generic RIFX container", "Macromedia Director Shockwave Cast", /^fmt\/(317|486)( |$)/, /^x-fmt\/341( |$)/];
 	weakMagic      = ["Generic RIFX container"];
-	
-	auxFiles = (input, otherFiles, otherDirs) =>
-	{
-		const xtrasDir = otherDirs.find(otherDir => otherDir.name.toLowerCase()==="xtras");
-		return xtrasDir ? [xtrasDir] : false;
-	};
-	notes      = "While 'xtras' is included here, it is NOT copied over into Windows with macromediaDirector. See more details in program/archive/macromediaDirector.js";
-	converters = [
+	converters     = [
 		// Director CastRipper has fully replaced macromediaDirector.js
-		// Some files must be unprotected first in order to get "all" the files, thus projectorrays goes first (DREAM3.DXR for example, without projectorrays then cast ripper only gets 14 files, decompiling it first gets 24 total)
+		// Some files must be unprotected first in order to get "all" the files (such as the lingo scripts), thus projectorrays goes first (DREAM3.DXR for example, without projectorrays then cast ripper only gets 14 files, decompiling it first gets 24 total)
 		"projectorRays -> directorCastRipper12",
 
-		// Next it's best to go straight to directorCastRipper12, which will handle casts more directly
-		"directorCastRipper12",
+		// Next, try going straight to directorCastRipper12, which will handle cast files more directly
+		"directorCastRipper12"
 
-		// Next try dirOpener which will not only un-protects it but wraps it in a modern up to date version of director for use
-		// We've been warned that dirOpener can mess up files it generates, cutting off sounds and such, thus it's a last resort here
-		"dirOpener -> directorCastRipper12"
+		/* Other extractors are available. Some were tried in the past (see sandbox/legacy/program/), some haven't been:
+		macromediaDirector	Used to use this for all extraction, but now thanks to Director Cast Ripper, it's not needed anymore
 
-		// recover_cct"		// Some .cct files (T4.cct) can be un-proceted to cast .cst files that can then be opened
-
-		/* Other extractors are available. Some were tried in the past, some haven't been:
 		SCUMMVM		https://github.com/scummvm/scummvm/tree/master/engines/director
 					Has great director support including extracting director files out of EXEs, but no CLI extraction tool. In theory could create one, but it looks like it'd be a LOT of work.
 					https://github.com/scummvm/scummvm/blob/master/engines/director/resource.cpp
@@ -40,6 +29,11 @@ export class macromediaDirector extends Format
 		
 		dirry		https://github.com/markhughes/dirry
 					Couldn't actually get it to extract anything, but it 'looked' like it was trying. Also no longer being updated.
+		
+		recover_cct	Used to use this to recover certain .cct files (T4.cct), but now Director Cast Ripper handles them fine
+
+		dirOpener	This tool used to not only un-protect a file, but also re-wrapped it in a modern up to date version of director, though sometimes would mess up some assets.
+					It was still an amazingly useful tool to use until Director Cast Ripper was created and now isn't needed at all
 		*/
 	];
 }

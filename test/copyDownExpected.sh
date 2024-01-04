2f46e96f240793b9053c4ecdf5a3b204788c7ce7
Robert Schultz <robert@cosmicrealms.com>
2024-01-04 18:52:31 -0500

    Updated for latest programs

---
+++ test/copyDownExpected.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+scp sembiance@dexdrone1:/mnt/compendium/DevLab/dexvert/test/testExpected.json /mnt/compendium/DevLab/dexvert/test/testExpected.json
--- util/printSetupSteps.js
+++ util/printSetupSteps.js
@@ -62,5 +62,6 @@ const programPackages = Object.values(programs).flatMap(program => Array.force(p
 	`mkdir /mnt/dexvert`,
 	`chown sembiance:sembiance /mnt/dexvert`,
 	`usermod -aG vboxusers sembiance`,
-	`cd /usr/lib64 && ln -s libimagequant.so libimagequant.so.0`	// required for uniconvertor
+	`cd /usr/lib64 && ln -s libimagequant.so libimagequant.so.0`,	// required for uniconvertor
+	`cd /usr/bin && ln -s scribus-1.6 scribus`	// require for scribus
 ].forEach(line => console.log(line));

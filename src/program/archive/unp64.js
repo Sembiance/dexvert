85a3569700c6aa186629a6b52156ac1446c8981f
Robert Schultz <robert@cosmicrealms.com>
2024-08-16 12:56:05 -0400

    wip

--- src/program/archive/unp64.js
+++ src/program/archive/unp64.js
@@ -3,11 +3,14 @@ import {Program} from "../../Program.js";
 
 export class unp64 extends Program
 {
-	website       = "https://iancoog.altervista.org/";
-	package       = "app-arch/unp64";
-	bin           = "unp64";
-	args          = r => [r.inFile()];
-	cwd           = r => r.outDir();
-	mirrorInToCWD = true;
-	renameOut     = true;
+	website        = "https://iancoog.altervista.org/";
+	package        = "app-arch/unp64";
+	bin            = "unp64";
+	args           = r => [r.inFile()];
+	cwd            = r => r.outDir();
+	mirrorInToCWD  = true;
+	checkForDups   = true;
+	unsafe         = true;
+	forbidChildRun = true;
+	renameOut      = true;
 }

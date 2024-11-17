dc1f09f98efd786aa41d9325bb6ba808cd31906a
Robert Schultz <robert@cosmicrealms.com>
2024-11-16 19:37:08 -0500

    fixed infinite recursion bug with mp3s

--- src/format/audio/mp3.js
+++ src/format/audio/mp3.js
@@ -7,7 +7,6 @@ export class mp3 extends Format
 	ext            = [".mp3", ".mpga", ".mpg"];
 	forbidExtMatch = [".mpg"];
 	mimeType       = "audio/mpeg";
-	priority       = this.PRIORITY.LOW;
 	magic          = [
 		// general MP3 magic
 		"LAME encoded MP3 audio", /^Audio file.* layer III/, "MPEG ADTS, layer III", "MPEG 1/2 Audio Layer 3", "Audio file with ID3 version", /^ID3v2.\d.0 Tag/, /^MP3 ID3 tag, v2\.\d$/, "audio/mpeg", "MP2/3 (MPEG audio layer 2/3) (mp3)",

94f0106ae0f28a657e1312a30e1ad8ef50b8d5ec
Robert Schultz <robert@cosmicrealms.com>
2025-05-02 22:32:06 -0400

    Fixed issue with infinite recursion of a particular .sea file

--- src/format/archive/sea.js
+++ src/format/archive/sea.js
@@ -9,4 +9,19 @@ export class sea extends Format
 	weakMagic  = ["Macintosh Application (MacBinary)", "Preferred Executable Format"];
 	idMeta     = ({macFileType, macFileCreator}) => macFileType==="APPL" && macFileCreator==="EXTR";
 	converters = ["unar[mac]"];
+	verify     = ({dexState, newFile}) =>
+	{
+		// some files like archive/sea/QReader.sea will infinite loop extract over and over. But unar throws an error in this case, so can check for that
+		const unrarRan = dexState.ran.find(({programid}) => programid==="unar");
+		if(!unrarRan || !unrarRan?.stdout?.includes("Archive parsing failed!"))
+			return true;
+
+		if(dexState.f?.files?.new?.length!==1)
+			return true;
+
+		if(newFile.base!==unrarRan.originalInput.base)
+			return true;
+
+		return false;
+	};
 }

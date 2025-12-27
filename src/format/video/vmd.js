a1239769d1a2015d28bbbbda1dcb1f5fd1ef04cb
Benjamin Funke <bjnfne@web.de>
2025-12-27 11:14:04 +0100

    refactor vmd format javascript file

--- src/format/video/vmd.js
+++ src/format/video/vmd.js
@@ -1,11 +1,11 @@
 import {Format} from "../../Format.js";
 
-export class sierraVMD extends Format
+export class vmd extends Format
 {
-	name         = "Sierra Video and Music Data";
+	name         = "Video and Music Data";
 	website      = "https://wiki.multimedia.cx/index.php/VMD";
 	ext          = [".vmd"];
-	magic        = ["Sierra Video and Music Data video", "Sierra VMD (vmd)"];
+	magic        = ["Video and Music Data", "VMD (vmd)"];
 	metaProvider = ["mplayer"];
 	converters   = ["ffmpeg[format:vmd]", "nihav"];
 }
